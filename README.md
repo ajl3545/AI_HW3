badf185e35599462054c6f60d4d7b13efd2b97ad
Alex Lamarche <ajl3545@rit.edu>
2021-11-14 18:55:01 -0500

    instructions

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # AI Homework 3
 
+Setup
+
+1. Download repo
+2. Install using the following commands
+
+pip3 install pandas
+pip3 install openpyxl 
+
 ### Problems 1-3
 Problem 1 = setting up the linear regression functions for other problems
 
--- problem2.py
+++ problem2.py
@@ -1,2 +1,14 @@
 # Alex Lamarche
 # AI EXP HW3 : Problem 2 : problem2.py
+
+import pandas as pd
+
+def load_table(table,sheet):
+    data = pd.read_excel(table,sheet_name=sheet)
+    return data 
+
+clean_tr_data = load_table("data/clean_data.xlsx","Sheet1")
+clean_v_data = load_table("data/clean_data.xlsx","Sheet2")
+clean_te_data = load_table("data/clean_data.xlsx","Sheet3")
+
+print(clean_tr_data)
