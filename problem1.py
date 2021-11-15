652f88a55c9e34d34386791bd24ebd7b7ea771ef
Alex Lamarche <ajl3545@rit.edu>
2021-11-15 18:12:17 -0500

    figs

--- problem1.py
+++ problem1.py
@@ -3,35 +3,42 @@
 
 import numpy as np
 
+
 def LIN_REG(X, w):
     # Augmented
-    A = np.append(1,X)
-    y = 0 
+    A = np.append(1, X)
+    y = 0
     for i in range(len(A)):
-        y += A[i]*w[i]  
+        y += A[i]*w[i]
     return y
 
 # X = (N x D) matrix of attribs
 # y = corresponding prediction vector
 # w = (D+1 x 1) parameter vector
+
+
 def MSE(X, y, w):
     se = SE(X, y, w)
     return se/len(X[0])
+
+
 def SE(X, y, w):
     total = 0  # Total Squared Error
     c = 0  # counter
     for attribs in X:
         total += pow(abs(y[c] - LIN_REG(attribs, w)), 2)
         c += 1
-    return total # Squared Error
+    return total  # Squared Error
 
-# Returns the cost function given any 
+# Returns the cost function given any
 # x,y,w parameters with l regularization
+
+
 def REG_MET(x, y, w, l):
 
     # L(w; X, y)= Loss
-    L = MSE(x, y, w)
-    
+    L = SE(x, y, w)
+
     # Euclidean norm of w = R(w)
     l_R = np.linalg.norm(w)**2
 
@@ -39,62 +46,70 @@ def REG_MET(x, y, w, l):
     return L + (l*l_R)
 
 # Returns the optimal cost function of a weighted w vector
+
+
 def CF_SOLVER(X, y, l):
 
     # Augment
-    A = [] # [1,X]
+    A = []  # [1,X]
     for row in X:
-        A.append(np.append(1,row))
+        A.append(np.append(1, row))
     A_tr = np.array(A)
     A_trans = np.transpose(A_tr)
     y_tr = y
 
-    # Wopt = (A_transpose * A + lI)^-1 * A_transpose * y_tr    
-    d = np.dot(A_trans,A_tr)
+    # Wopt = (A_transpose * A + lI)^-1 * A_transpose * y_tr
+    d = np.dot(A_trans, A_tr)
     ident = l*np.identity(len(d))
     inv = np.linalg.inv(d + ident)
-    Wopt = np.dot(inv,np.dot(A_trans,y_tr))
+    Wopt = np.dot(inv, np.dot(A_trans, y_tr))
     Wopt_t = np.transpose(Wopt)
 
-    return (Wopt,REG_MET(X,y,Wopt_t,l))
+    return (Wopt, REG_MET(X, y, Wopt_t, l))
 
 
 def GD_SOLVER(X, y, p, l, step):
 
-    parameters = []; parameters.append(p)
+    parameters = []
+    parameters.append(p)
     costs = []
+    norms = []
 
-    i = 1 
-    
+    i = 1
+
     while (True):
-        
+
         # Most recent W
         w = parameters[-1]
 
         (mtr) = REG_MET(X, y, w, l)
         costs.append(mtr)
 
-        g = gradient(X,w,y,l)
+        g = gradient(X, w, y, l)
+
+        e_n = np.linalg.norm(g)**2
+        norms.append(e_n)
 
         # Terminate
-        if (np.linalg.norm(g)**2 < pow(10,-8)):
-            print("W len = " + str(len(parameters))) 
-            print("iters len = " + str(i)) 
-            return (parameters,costs)
-        
+        if (e_n < pow(10, -8)):
+            print("W len = " + str(len(parameters)))
+            print("iters len = " + str(i))
+            return (parameters, costs, norms)
+
         # Descend gradient
         parameters.append(w - step*g)
 
-        i+=1
+        i += 1
 
-def gradient(X,w,y,l):
+
+def gradient(X, w, y, l):
     A = []
     for row in X:
-        A.append(np.append(1,row))
+        A.append(np.append(1, row))
     A_tr = np.array(A)
     A_trans = np.transpose(A_tr)
 
-    d = np.dot(A_trans,A_tr)
+    d = np.dot(A_trans, A_tr)
     ident = l*np.identity(len(d))
 
-    return (2 * np.dot(d+ident,w)) - (2 * np.dot(A_trans,y))
+    return (2 * np.dot(d+ident, w)) - (2 * np.dot(A_trans, y))
--- problem2.py
+++ problem2.py
@@ -1,9 +1,12 @@
 # Alex Lamarche
 # AI EXP HW3 : Problem 2 : problem2.py
 
+from numpy.core.fromnumeric import transpose
 import pandas as pd
 import numpy as np
 import problem1 as p1
+
+import matplotlib.pyplot as plt
 
 
 def load_table(table, sheet):
@@ -11,7 +14,7 @@ def load_table(table, sheet):
     return data
 
 
-def execute(data):
+def execute(data, c):
 
     # Get the X_tr matrix
     X_tr = data.iloc[:, 1:].values.tolist()
@@ -29,13 +32,23 @@ def execute(data):
     print("Opt MSE = " + str(mse))
 
     # 2.3 Intermediate MSE's during descent
-    (W, costs) = p1.GD_SOLVER(X_tr, y_tr, np.ones(len(X_tr[0])+1), 0, 0.01)
+    (W, costs, norms) = p1.GD_SOLVER(
+        X_tr, y_tr, np.ones(len(X_tr[0])+1), 0, 0.001)
     mse_count = 0
+
+    ms = []
+
     for w_params in W:
         m = p1.MSE(X_tr, y_tr, w_params)
+        ms.append(m)
         m_s = "{:.10f}".format(m)
         print("MSE " + str(mse_count) + " = " + m_s)
         mse_count += 1
+
+    # plot the points
+
+    plt.plot(np.transpose([range(len(W))]), ms, color=c)
+    plt.plot(np.transpose([range(len(W))]), [mtr]*len(W), color="black")
 
 # For the first 10 values
 
@@ -99,9 +112,10 @@ clean_v_data = load_table("data/clean_data.xlsx", "Sheet2")
 clean_te_data = load_table("data/clean_data.xlsx", "Sheet3")
 
 # Training executions
-execute(clean_tr_data)           # all data
+execute(clean_tr_data, "blue")           # all data
 # execute_first_10(clean_tr_data) # first 10 rows
 # execute_l_2(clean_tr_data)      # lambda = 2
 
 # Testing executions
-# execute(clean_te_data)          # all data
+execute(clean_te_data, "red")          # all data
+plt.show()
